d673849d96a4ef51b466187de2552a2285c3e32d
Dmitry Irlitsa <irlitsa.d.p@gmail.com>
2022-07-05 01:03:15 +0300

    Update project, add tasks asyncio 01-02

--- multithreading/decorators.py
+++ multithreading/decorators.py
@@ -2,6 +2,18 @@ from functools import wraps
 import time
 
 
+def async_measure_time(func):
+    @wraps(func)
+    async def wrap(*args, **kwargs):
+        start = time.perf_counter()
+        result = await func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        print(f'Executed {func} in {elapsed:0.2f} seconds')
+        return result
+
+    return wrap
+
+
 def measure_time(func):
     @wraps(func)
     def wrap(*args, **kwargs):
